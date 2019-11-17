01fb98cf4fe72f8d2618427f95d2f98075ea589d
Lucien-BR <54720026+Lucien-BR@users.noreply.github.com>
2019-11-17 14:16:39 -0500

    Building the post methods

--- src/index.js
+++ src/index.js
@@ -9,17 +9,27 @@ const pool = new Pool({
     connectionString: connectionString,
 });
 
+const user = [{email:'luc00110@gmail.com', nom:'Blais Regout', prenom:'Lucien', telephone:'(581)-998-5811', status:'A' }];
+
 function getAllUsers(){
     pool.query('SELECT * FROM Users', (err, res) => {
-        console.log(err, res);
-        pool.end();
+        
+        console.log(err, res.rows);
+        //pool.end();
+        return (err, res.rows);
     });
+
+}
+
+function addUser() {
+    pool.query('INSERT INTO Users')
+
 }
 
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
-        extended: true,
+        extended: false,
     })
 );
 
@@ -27,6 +37,24 @@ app.get('/', (request, response) => {
     response.json({info: 'Node.js, Express, PG'})
 });
 
-app.listen(port, () => {
+app.post('/plusone',function(req,res){
+    var email     = req.body.email,
+        nom       = req.body.nom,
+        prenom    = req.body.password,
+        telephone = req.body.telephone,
+        status    = req.body.status;
+    
+    console.log("User name = "+user_name+", password is "+password);
+    res.end("yes");
+  });
+
+  app.get("/users", function(req,res) {
+        
+        //user.push(req.body);
+        res.json(getAllUsers());
+  })
+
+
+  app.listen(port, () => {
     console.log('Listening on port 3000.')
 });
