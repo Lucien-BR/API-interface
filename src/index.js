70a49672ac482732226bd6be8290139d17de2499
Lucien-BR <54720026+Lucien-BR@users.noreply.github.com>
2019-11-17 12:33:16 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const { Pool } = require('pg');
+const connectionString = 'psotgresql://Lucien:lu-db@35.245.152.215:5432/impro-bd';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    connectionString: connectionString,
+});
+function getAllUsers(){
+    pool.query('SELECT * FROM Users', (err, res) => {
+        console.log(err, res);
+        pool.end();
+    });
+}
+/*
+const client = new Client({
+    connectionString: connectionString,
+});
+client.connect();
+
+client.query('SELECT * FROM Users', (err, res) => {
+    console.log(err, res);
+    client.end();
+})
+*/
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get('/', (request, response) => {
+    response.json({info: 'Node.js, Express, PG'})
+});
+
+app.listen(port, () => {
+    console.log('Listening on port 3000.')
+});
