40bd8c3f4bb98382ab16ffec6559b1954a758036
Lucien-BR <54720026+Lucien-BR@users.noreply.github.com>
2019-11-18 23:31:49 -0500

    New DB. new API support
    API now supports:
     - default gateway ('/') now redirect to a routing guide (quick How-To) in .json format.
     - Users Table [UPDATE, DELETE, INSERT, SELECT]
     - Credentials Table [UPDATE (status/psw), DELETE, INSERT, SELECT]

--- src/MyPostgres.js
+++ src/MyPostgres.js
@@ -9,27 +9,29 @@ class MyPostgres {
   }
 
   async getAllUsers() {
-      var temp; // hmm
+      var code = 0, temp;
       const client = await this.pool.connect();
       await client
         .query('SELECT * FROM Users')
-        .then(result => temp = result) // probablement redondant
-        .catch(e => console.error(e.stack));
-
-      /*
-      this.client.connect();
-      await this.client
-        .query("SELECT * FROM Users")
         .then(result => temp = result)
-        .catch(e => console.error(e.stack))
-        .then(() => this.client.end());
-        
-      */
-      return  temp;
+        .catch(e => {console.error(e.stack); code = 1;});
+      client.release();
+      return  [code, temp];
   }
 
-  removeUser(email) {
-      var state = 0;
+  async getAllCreds() {
+    var code = 0, temp;
+    const client = await this.pool.connect();
+    await client
+      .query('SELECT * FROM Credentials')
+      .then(result => temp = result) // prob. redondant
+      .catch(e => {console.error(e.stack); code = 1;});
+    client.release();
+    return  [code, temp];
+  }
+
+  async removeUser(email) {
+      var er = null, code =0;
       ;(async () => {
           const client = await this.pool.connect();
           try {
@@ -39,42 +41,151 @@ class MyPostgres {
             await client.query(queryText, userValue);
             await client.query('COMMIT');
           } catch (e) {
-            state = 1;
             await client.query('ROLLBACK');
+            code = 1;
             throw e;
           } finally {
             client.release();
           }
-        })().catch(e => console.error(e.stack));
-      return state;
+        })().catch(e => {console.error(e.stack); er = e});
+      return [code, this.errMessage(er)];
   }
 
-  addUser(email, nom, prenom, telephone, status) {
-      var state = 0;
+  async removeCred(email) {
+    var er = null, code =0;
+    ;(async () => {
+        const client = await this.pool.connect();
+        try {
+          await client.query('BEGIN');
+          const queryText = 'DELETE FROM Credentials * WHERE email = $1';
+          const userValue = [email];
+          await client.query(queryText, userValue);
+          await client.query('COMMIT');
+        } catch (e) {
+          await client.query('ROLLBACK');
+          code = 1;
+          throw e;
+        } finally {
+          client.release();
+        }
+      })().catch(e => {console.error(e.stack); er = e});
+    return [code, this.errMessage(er)];
+}
+
+  async addUser(email, nom, prenom, telephone, status) {
+      var er = null, code = 0;
       ;(async () => {
           const client = await this.pool.connect();
           try {
             await client.query('BEGIN');
             const queryText = 'INSERT INTO Users(email, nom, prenom, telephone, status) VALUES($1, $2, $3, $4, $5)';
-            const userValues = [email, nom, prenom, telephone, status];
-            await client.query(queryText, userValues);
+            const queryValues = [email, nom, prenom, telephone, status];
+            await client.query(queryText, queryValues);
             await client.query('COMMIT');
           } catch (e) {
-            state = 1;
+            code = 1;
             await client.query('ROLLBACK');
             throw e;
           } finally {
             client.release();
           }
-        })().catch(e => console.error(e.stack));
-      return state;
+        })().catch(e => {console.error(e.stack); er = e});
+      return [code, this.errMessage(er)];
   }
 
-  errMessage(state) {
-      if (state == 0){    
-          return("\nTask completed succesfully.\n");
-      } else {
-          return("\nTask failed, rolling back. Modifications were reverted.\n");
-      }
+  async addCred(email, psw, status) {
+    var er = null, code = 0;
+    ;(async () => {
+        const client = await this.pool.connect();
+        try {
+          await client.query('BEGIN');
+          const queryText = 'INSERT INTO Credentials(email, psw, status) VALUES($1, $2, $3)';
+          const queryValues = [email, psw, status];
+          await client.query(queryText, queryValues);
+          await client.query('COMMIT');
+        } catch (e) {
+          code = 1;
+          await client.query('ROLLBACK');
+          throw e;
+        } finally {
+          client.release();
+        }
+      })().catch(e => {console.error(e.stack); er = e});
+    return [code, this.errMessage(er)];
+  }
+
+  async updateUser(email, nom, prenom, telephone, status) {
+      var er = null, code = 0;
+      ;(async () => {
+          const client = await this.pool.connect();
+          try {
+            await client.query('BEGIN');
+            const queryText = 'UPDATE Users SET nom = $1, prenom = $2, telephone = $3, status = $4 WHERE email = $5';
+            const queryValues = [nom, prenom, telephone, status, email];
+            await client.query(queryText, queryValues);
+            await client.query('COMMIT');
+          } catch (e) {
+            await client.query('ROLLBACK');
+            code = 1;
+            throw e;
+          } finally {
+            client.release();
+          }
+        })().catch(e => {console.error(e.stack); er = e});
+      return [code, this.errMessage(er)];
+  }
+
+  // psw change
+  async updateCredPsw(email, psw) {
+    var er = null, code = 0;
+    ;(async () => {
+        const client = await this.pool.connect();
+        try {
+          await client.query('BEGIN');
+          const queryText = 'UPDATE Credentials SET psw = $1 WHERE email = $2';
+          const queryValues = [psw, email];
+          await client.query(queryText, queryValues);
+          await client.query('COMMIT');
+        } catch (e) {
+          await client.query('ROLLBACK');
+          code = 1;
+          throw e;
+        } finally {
+          client.release();
+        }
+      })().catch(e => {console.error(e.stack); er = e});
+    return [code, this.errMessage(er)];
+  }
+
+  // status change
+  async updateCredStatus(email, status) {
+    var er = null, code = 0;
+    ;(async () => {
+        const client = await this.pool.connect();
+        try {
+          await client.query('BEGIN');
+          const queryText = 'UPDATE Credentials SET status = $1 WHERE email = $2';
+          const queryValues = [status, email];
+          await client.query(queryText, queryValues);
+          await client.query('COMMIT');
+        } catch (e) {
+          await client.query('ROLLBACK');
+          code = 1;
+          throw e;
+        } finally {
+          client.release();
+        }
+      })().catch(e => {console.error(e.stack); er = e});
+    return [code, this.errMessage(er)];
+  }
+
+  errMessage(er) {
+    if (er == null){    
+      return("Task completed succesfully.\n");
+    } 
+    else {
+      console.log(er.stack);
+      return("Task failed, rolling back. Modifications were reverted.\n\n Have fun with the stack tracer! \n\n"+er.stack);
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,45 +6,90 @@ const port          = 8080;
 
 const MyPG = new MyPostgres();
 
-app.use(bodyParser.text({ type: 'text/html' })); // principalement celui-ci.
+app.use(bodyParser.text({ type: 'text/html' })); // Mainly this one
 app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(bodyParser.json());
-// Ne pas confondre err/res du module EXPRESS avec ceux du modul PG
 
-// Faire dequoi avec le default one.. idk
-app.get('/test', (request, response) => {
-    response.json({info: 'Ceci fonctionne!'})
+// Do something with the default one.. idk
+app.get('/', (request, response) => {
+    response.status(200).json(
+        {info: 'Voici notre API!', routes: {
+            GET: {
+                info: '/',
+                Voir_Tout_Utilisateurs: '/users',
+                Voir_Tout_Identifiants: '/creds'                
+            },
+            POST: {
+                Ajouter_Utilisateur: '/adduser/<email>/<nom>/<prenom>/<telephone>/<status>/<motDePasse>',
+                Metre_A_Jour_Utilisateur: '/updateuser/<email>/<nom>/<prenom>/<telephone>/<status>',
+                Retirer_Utilisateur: '/removeuser/<email>',
+                Changer_Mot_De_Passe: '/updatepsw/<email>/<nouveauMotDePasse>'
+            }
+        }
+    })
 });
-
-fetchAllUser = async function() {
-    let returnData = [];
-    returnData = await MyPG.getAllUsers();
-    //console.log(returnData); // TOUT
-    return returnData;
-}
 
 app.get("/users", async (req,res) => {
-    let returnData = await fetchAllUser();
-    console.log(returnData); // fkall
-    res.status(200).json({ users: returnData.rows });
+    let pgRes = await MyPG.getAllUsers();
+    var code        = 200;
+    if (pgRes[0] != 0) { code = 400; }
+    res.status(code).json({ users: pgRes[1].rows });
 });
 
-app.get('/adduser/:email/:nom/:prenom/:telephone/:status', (req,res) => {
+app.get("/creds", async (req,res) => {
+    let pgRes = await MyPG.getAllCreds();
+    var code        = 200;
+    if (pgRes[0] != 0) { code = 400; }
+    res.status(code).json({ users: pgRes[1].rows });
+});
+
+app.post('/adduser/:email/:nom/:prenom/:telephone/:status/:psw', async (req,res) => {
+    var email       = req.params.email;
+    var nom         = req.params.nom;
+    var prenom      = req.params.prenom;
+    var telephone   = req.params.telephone;
+    var status      = req.params.status; // defaults to 'D'
+    var psw         = req.params.psw;
+    let pgRes       = await MyPG.addUser(email, nom, prenom, telephone, status);
+    let pgRes2      = await MyPG.addCred(email, psw, status);
+    var code        = 201;
+    if (pgRes[0] != 0 && pgRes2[0] != 0) { code = 406;}
+    res.status(code).end("\n"+code+" "+pgRes2[1]); //.2 because it can't compile if the first one cannot.
+});
+
+// NEVER modify the email. This api won't allow it.
+app.post('/updateuser/:email/:nom/:prenom/:telephone/:status', async (req,res) => {
     var email       = req.params.email;
     var nom         = req.params.nom;
     var prenom      = req.params.prenom;
     var telephone   = req.params.telephone;
     var status      = req.params.status;
-    var state       = MyPG.addUser(email, nom, prenom, telephone, status);
-    res.end(MyPG.errMessage(state));
+    let pgRes       = await MyPG.updateUser(email, nom, prenom, telephone, status);
+    let pgRes2      = await MyPG.updateCredStatus(email, status);
+    var code        = 202;
+    if (pgRes[0] != 0 && pgRes2[0]) { code = 406; }
+    res.status(code).end("\n"+code+" "+pgRes2[1]);
 });
 
-app.get('/removeuser/:email', (req, res) => {
+app.post('/updatepsw/:email/:psw', async (req,res) => {
     var email       = req.params.email;
-    var state       = MyPG.removeUser(email);
-    res.end(MyPG.errMessage(state));
+    var psw         = req.params.psw;
+    let pgRes       = await MyPG.updateCredPsw(email, psw);
+    var code        = 202;
+    if (pgRes[0] != 0) { code = 406; }
+    res.status(code).end("\n"+code+" "+pgRes[1]);
 });
 
+app.post('/removeuser/:email', async (req, res) => {
+    var email       = req.params.email;
+    let pgRes       = await MyPG.removeCred(email);
+    let pgRes2      = await MyPG.removeUser(email);
+    var code        = 202;
+    if (pgRes[0] != 0 && pgRes2[0] != 0) { code = 406; }
+    res.status(code).end("\n"+code+" "+pgRes2[1]);
+});
+
+// Basicly allows this api to wait for requests.
 app.listen(port, () => {
-  console.log('Listening on port '+port+'.')
+  console.log('Listening on http://localhost:'+port);
 });
